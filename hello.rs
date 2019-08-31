1e8bdb219f128c7d9c42ee4e24f6614dfb37b2a8
Dimiter 'malkia' Stanev <malkia@gmail.com>
2019-08-30 23:31:46 -0700

    I'm about to raze a cargo cult, and vendor everything. No really I don't know what I'm doing, I was simply following the steps here - https://github.com/google/cargo-raze

--- hello.rs
+++ hello.rs
@@ -1,3 +1,16 @@
+extern crate string_interner;
+use string_interner::StringInterner;
+
 fn main() {
     println!("hello");
+
+    let mut interner = StringInterner::default();
+    let sym0 = interner.get_or_intern("Elephant");
+    let sym1 = interner.get_or_intern("Tiger");
+    let sym2 = interner.get_or_intern("Horse");
+    let sym3 = interner.get_or_intern("Tiger");
+    assert_ne!(sym0, sym1);
+    assert_ne!(sym0, sym2);
+    assert_ne!(sym1, sym2);
+    assert_eq!(sym1, sym3); // same!
 }
